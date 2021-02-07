129a6c4d21de2796685415dfa3e77788cba844c4
Reuben T Allik <reubenallik@gmail.com>
2021-02-06 22:41:44 -0500

    2.6

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req,res) => {
     res.send("Home page")
 });
 
-
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
     console.log("lkjahsdfuhasdf")
 })
